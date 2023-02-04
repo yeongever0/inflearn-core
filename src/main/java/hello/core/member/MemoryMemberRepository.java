75ce5cec7ca88f3fe8f782a9aba939d91ff6a26e
hwangjiyeong <gyp0288@naver.com>
2023-02-05 00:05:13 +0900

    add: 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    //HashMap은 동시성 이슈가 있음, 실무에선 ConcurrentHashMap 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
