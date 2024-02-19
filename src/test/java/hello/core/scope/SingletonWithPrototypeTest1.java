8349558facefe77a1e189779e28bddf47001a79a
yeongever0 <yeongever0@gmail.com>
2024-02-19 22:51:02 +0900

    test: Prototype Scope를 Singleton Bean과 함께 사용 시 `Provider`로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
+import javax.inject.Provider;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
+class SingletonWithPrototypeTest1 {
 
     @Test
     void prototypeFind() {
@@ -27,24 +29,22 @@ public class SingletonWithPrototypeTest1 {
     void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
-        int count1 = clientBean1.logig();
+        int count1 = clientBean1.logic();
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean2.logig();
+        int count2 = clientBean2.logic();
 
-        assertThat(count2).isEqualTo(2); //하나만 호출되므로 Test 통과됨
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; //생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        public int logig() {
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
